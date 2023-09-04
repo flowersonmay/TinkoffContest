37b60adca2e706493093e1ef0c8ff950227d560f
leskov <flowersonmay@gmail.com>
2023-09-04 17:46:26 +0300

    add task 2

--- src/Task2.java
+++ src/Task2.java
@@ -1,6 +1,9 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.Scanner;
 
-public class Main {
+public class Task2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         //sheriff
